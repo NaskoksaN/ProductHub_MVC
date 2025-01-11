1413f4bf9b20cc7c4ea1bb9474f5cc607bd3bb19
NaskoksaN <nbobchev@yahoo.com>
2025-01-11 14:17:24 +0200

    Create Configuration Folder and add CategoryConfiguration class.

---
+++ ProductWeb/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductWeb.Models.DataModels;
+using System.Reflection.Emit;
+
+namespace ProductWeb.Data.Configuration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = 1, Name = "Soap", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Shower Gel", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Premium Soap", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Shampoo", DisplayOrder = 4 }
+                );
+        }
+    }
+}
