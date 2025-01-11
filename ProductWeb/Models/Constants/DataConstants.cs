96d9196a8c23f9c5d028e4efc3913c399b07f764
NaskoksaN <nbobchev@yahoo.com>
2025-01-11 18:08:25 +0200

    Add more constants

---
+++ ProductWeb/Models/Constants/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductWeb.Models.Constants
+{
+    public static class DataConstants
+    {
+        public const int CategoryNameMaxLength = 100;
+        public const int CategoryNameMinLength = 1;
+
+        public const int DipslayOrderMaxValue = 100;
+        public const int DisplayOrderMinValue = 1;
+    }
+}
---
+++ ProductWeb/Models/Constants/MessageConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductWeb.Models.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "The {0} is required.";
+        public const string LengthMessage = "The {0} must be between {2} and {1} characters long.";
+        public const string RangeMessage = "The {0} must be between {1} and {2}. Keep in mind.";
+        public const string NameAndDiplayorderMatch = "Display Order cannot exactly match Name";
+
+        public const string ItemCreated = "Item is created successfully.";
+        public const string ItemUpdate = "Item is update successfully.";
+        public const string ItemDelete = "Item is delete successfully.";
+
+        public const string ErrorCreated = "Item is created successfully.";
+        public const string ErrorUpdate = "Item is update successfully.";
+        public const string ErrorDelete = "Item is delete successfully.";
+        public const string ItemNotFound = "Item not found";
+    }
+}
