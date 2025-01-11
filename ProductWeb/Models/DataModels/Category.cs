da650074f862ad795baf57f4c83591224530c348
NaskoksaN <nbobchev@yahoo.com>
2025-01-11 12:49:15 +0200

    Create static DataConstants class

---
+++ ProductWeb/Models/DataModels/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ProductWeb.Models.DataConstants;
+
+namespace ProductWeb.Models.DataModels
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(CategoryNameMaxLength)]
+        public string Name { get; set; }=string.Empty;
+        public int DisplayOrder { get; set; }
+    }
+}
