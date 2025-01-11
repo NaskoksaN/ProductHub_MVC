532e43fe66fe53c62456bf913be06923fdc1023b
NaskoksaN <nbobchev@yahoo.com>
2025-01-11 13:47:08 +0200

    Add in Extension folder SeriviceColelctionExtension and public static IServiceCollection AddApplicationDbContex to configure database sql server

---
+++ ProductWeb/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductWeb.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration
+                        .GetConnectionString("DefaultConnection") 
+                        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+
+    }
+}
