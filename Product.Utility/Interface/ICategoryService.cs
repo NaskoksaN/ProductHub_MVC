80e959f403d69eb30006f2db755ac016b400d349
NaskoksaN <nbobchev@yahoo.com>
2025-01-12 12:22:49 +0200

    Interface inherit ISqlRepository and implement Update and SaveAsync in Catgeroy service

---
+++ Product.Utility/Interface/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Product.DataAccess.Entities;
+using Product.DataAccess.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Utility.Interface
+{
+    public interface ICategoryService : ISqlRepository<Category>
+    {
+        void Update(Category obj);
+        Task SaveAsync();
+    }
+}
---
+++ Product.Utility/Service/CatgeroyService.cs
@@ -0,0 +1,25 @@
+﻿using Product.DataAccess;
+using Product.DataAccess.Entities;
+using Product.DataAccess.Repository;
+using Product.Utility.Interface;
+
+namespace Product.Utility.Service
+{
+    public class CatgeroyService :SqlRepository<Category>, ICategoryService
+    {
+        private readonly ApplicationDbContext db;
+        public CatgeroyService(ApplicationDbContext _db) : base(_db)
+        {
+            db = _db;
+        }
+        public async Task SaveAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+
+        public void Update(Category obj)
+        {
+            db.Categories.Update(obj);
+        }
+    }
+}
