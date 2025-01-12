3d10eb2a8df72985fb966799c21a4cd45fa75443
NaskoksaN <nbobchev@yahoo.com>
2025-01-12 09:58:14 +0200

    Create folder in DataAccess Repository where i create ISqlRepository and implement in SqlRepository

---
+++ Product.DataAccess/Repository/Interface/ISqlRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Product.DataAccess.Repository.Interface
+{
+    public interface ISqlRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ Product.DataAccess/Repository/SqlRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.DataAccess.Repository.Interface;
+using System.Linq.Expressions;
+
+namespace Product.DataAccess.Repository
+{
+    public class SqlRepository<T> : ISqlRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext db;
+        internal DbSet<T> dbSet;
+        public SqlRepository(ApplicationDbContext _db)
+        {
+            db= _db;
+            this.dbSet= db.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+           await dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            IQueryable<T> query = dbSet;
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+               
+    }
+}
