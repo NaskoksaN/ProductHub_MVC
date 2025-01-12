5294ba135ffb198b7d56522ec246ed2c99880880
NaskoksaN <nbobchev@yahoo.com>
2025-01-12 13:56:54 +0200

    Add Product clas

---
+++ Product.DataAccess/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductHub.DataAccess.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Comment("Product name")]
+        public string Name { get; set; }
+        [Required]
+        [Comment("Product Description")]
+        [MaxLength(ProductDescriptionMaxLength)]
+        public string Description { get; set; }
+        
+        [Comment("Product price")]
+        public double Price {  get; set; }
+
+        public string ISBN {  get; set; } 
+    }
+}
