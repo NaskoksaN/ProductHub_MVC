a41d717de2e01c42b6e2a1b7ff443fd1a7bc5e5b
NaskoksaN <nbobchev@yahoo.com>
2025-01-16 18:47:17 +0200

    Add constants for Products

--- Product.Models/Constants/DataConstants.cs
+++ Product.Models/Constants/DataConstants.cs
@@ -10,5 +10,15 @@
 
         public const int ProductDescriptionMaxLength = 2000;
         public const int ProductDescriptionMinLength = 1;
+
+        public const int ProductNameMaxLength = 100;
+        public const int ProductNameMinLength = 1;
+
+        public const int ProductAmountMaxValue = 10000;
+        public const int ProductAmountMinValue = 1;
+
+        public const double ProductPriceMaxValue = 10000.00;
+        public const double ProductPriceMinValue = 0.00;
+
     }
 }
