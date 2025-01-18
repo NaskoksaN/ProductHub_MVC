597aed4d1ab6d569cc554fd3691daf88e691ce05
NaskoksaN <nbobchev@yahoo.com>
2025-01-18 09:37:02 +0200

    Modify interface and class, AddAsync.

---
+++ Product.Utility/Service/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using ProductHub.DataAccess;
+using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository;
+using ProductHub.Utility.Interface;
+
+namespace ProductHub.Utility.Service
+{
+    public class ProductService : SqlRepository<Product>, IProductService
+    {
+        private readonly ApplicationDbContext db;
+        public ProductService(ApplicationDbContext _db) : base(_db)
+        {
+            db = _db;
+        }
+
+        public void Update(Product obj)
+        {
+            db.Products.Update(obj);
+        }
+    }
+}
