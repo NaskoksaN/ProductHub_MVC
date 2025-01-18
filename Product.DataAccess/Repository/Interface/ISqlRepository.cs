73e9d58761d04c33a7c81c4fa540df88841d337b
NaskoksaN <nbobchev@yahoo.com>
2025-01-18 11:52:28 +0200

    Add IncludeProperties in both GetAsync and GetAllAsync, can be included with ',' separated

--- Product.DataAccess/Repository/Interface/ISqlRepository.cs
+++ Product.DataAccess/Repository/Interface/ISqlRepository.cs
@@ -4,8 +4,8 @@ namespace ProductHub.DataAccess.Repository.Interface
 {
     public interface ISqlRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<IEnumerable<T>> GetAllAsync(string? includeProperties = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, string? includeProperties = null);
         Task AddAsync(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
