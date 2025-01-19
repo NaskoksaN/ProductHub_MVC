b5e925d88b7699e96852c1e226fa857b17ece401
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:53:00 +0200

    Refactor ApplicationUsers, i change name of dataconst .remove applicationsuer

---
+++ Product.DataAccess/Entities/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+using static ProductHub.Models.Constants.DataConstants;
+
+namespace ProductHub.DataAccess.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("User full name")]
+        public string Name {  get; set; }=string.Empty;
+
+        [MaxLength(StreetMaxLength)]
+        [Comment("User street address")]
+        public string? StreetAddress { get; set; }
+
+        [MaxLength(CityMaxLength)]
+        [Comment("User city")]
+        public string? City { get; set; }
+
+        [MaxLength(PostCodeMaxLength)]
+        [Comment("User postal code")]
+        public string? PostalCode {  get; set; }
+    }
+}
