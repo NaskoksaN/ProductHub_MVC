591e8b6f7dc648269e5728f079c9f56b476e9d3d
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:53:15 +0200

    remove applicationsuer from name, streetaddress..

--- Product.Models/Constants/DataConstants.cs
+++ Product.Models/Constants/DataConstants.cs
@@ -20,5 +20,22 @@
         public const double ProductPriceMaxValue = 10000.00;
         public const double ProductPriceMinValue = 0.00;
 
+        public const int NameMaxLength = 100;
+        public const int NameMinLength = 1;
+
+        public const int StreetMaxLength = 150;
+        public const int ApplicationUserStreetMinLength = 1;
+
+        public const int CityMaxLength = 70;
+        public const int ApplicationUserCityMinLength = 1;
+
+        public const int PostCodeMaxLength = 20;
+        public const int PostCodeMinLength = 1;
+
+        public const int PhoneMaxLength = 20;
+        public const int PhoneMinLength = 1;
+
+        public const int VATMaxLength = 20;
+        public const int VATMinLength = 1;
     }
 }
