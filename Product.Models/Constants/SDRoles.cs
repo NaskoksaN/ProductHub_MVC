869e3df89fd1da339f7ba41a8d6b266a766df1ae
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:53:32 +0200

    Create constant static class for Roles.

---
+++ Product.Models/Constants/SDRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductHub.Models.Constants
+{
+    public static class SDRoles
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
