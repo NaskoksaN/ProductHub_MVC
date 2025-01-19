eaa02685909fb055502c903df23a19fc9fb2b43a
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:51:43 +0200

    Create ICompanyService and implement in CompanyService

---
+++ Product.Utility/Interface/ICompanyService.cs
@@ -0,0 +1,10 @@
+﻿using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository.Interface;
+
+namespace ProductHub.Utility.Interface
+{
+    public interface ICompanyService : ISqlRepository<Company>
+    {
+        void Update(Company obj);
+    }
+}
---
+++ Product.Utility/Service/CompanyService.cs
@@ -0,0 +1,20 @@
+﻿using ProductHub.DataAccess.Data;
+using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository;
+using ProductHub.Utility.Interface;
+
+namespace ProductHub.Utility.Service
+{
+    public class CompanyService : SqlRepository<Company>, ICompanyService
+    {
+        private readonly ApplicationDbContext db;
+        public CompanyService(ApplicationDbContext _db) : base(_db)
+        {
+            db = _db;
+        }
+        public void Update(Company obj)
+        {
+            db.Companies.Update(obj);
+        }
+    }
+}
