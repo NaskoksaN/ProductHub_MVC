bcce44ae31d9b6df5ffba1bfb37af9bfc98cc060
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:58:40 +0200

    add identityuser, also role and cookies path in servic

--- ProductWeb/Extensions/ServiceCollectionExtension.cs
+++ ProductWeb/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ProductHub.DataAccess;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using ProductHub.DataAccess.Data;
 using ProductHub.Utility.Interface;
 using ProductHub.Utility.Service;
@@ -12,6 +13,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationService(this IServiceCollection services)
         {
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IEmailSender, EmailSender>();
             
             return services;
         }
@@ -25,6 +27,34 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(connectionString);
+              
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.
+                AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+
+                    //only fo development
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied" ;
+
             });
 
             return services;
