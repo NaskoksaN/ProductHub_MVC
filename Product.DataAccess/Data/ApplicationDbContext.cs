e07c27b06d0420ee644e7e55326082f07e07592d
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:52:22 +0200

    Add Dbset<Company>

--- Product.DataAccess/Data/ApplicationDbContext.cs
+++ Product.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace ProductHub.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
