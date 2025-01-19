0242f0021894625475c6d0bcbad3ef2ee62463cf
NaskoksaN <nbobchev@yahoo.com>
2025-01-19 13:52:08 +0200

    Add table to DB Company

---
+++ Product.DataAccess/Entities/Company.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+using static ProductHub.Models.Constants.DataConstants;
+
+namespace ProductHub.DataAccess.Entities
+{
+    public class Company
+    {
+        [Key]
+        [Comment("Company Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("Company Name")]
+        public string Name { get; set; }=string.Empty;
+
+        [Required]
+        [MaxLength(VATMaxLength)]
+        [Comment("Company VAT")]
+        public string VAT {  get; set; }= string.Empty;
+
+        [MaxLength(CityMaxLength)]
+        [Comment("Company City")]
+        public string? City { get; set; }
+
+        [MaxLength(PostCodeMaxLength)]
+        [Comment("Company Postal Code")]
+        public string? PostalCode {  get; set; }
+
+        [MaxLength(StreetMaxLength)]
+        [Comment("Company Street Address")]
+        public string? StreetAddress { get; set; }
+
+        [MaxLength(PhoneMaxLength)]
+        [Comment("Company Phone Number")]
+        public string? PhoneNumber { get; set; }
+    }
+}
