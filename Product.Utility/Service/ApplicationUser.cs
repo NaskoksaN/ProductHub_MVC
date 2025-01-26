babf796e9fdcaef07ef8475c208352cd2acea999
NaskoksaN <nbobchev@yahoo.com>
2025-01-26 09:00:33 +0200

    Create Interface and service

---
+++ Product.Utility/Service/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using ProductHub.DataAccess.Data;
+using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository;
+using ProductHub.Utility.Interface;
+
+namespace ProductHub.Utility.Service
+{
+    public class ApplicationUserService : SqlRepository<ApplicationUser>, IApplicationUserService
+    {
+        private readonly ApplicationDbContext db;
+        public ApplicationUserService(ApplicationDbContext _db) : base(_db)
+        {
+            db = _db;
+        }
+       
+    }
+}
