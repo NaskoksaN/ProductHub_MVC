7c1b42ca294fd8151d669d7ad360442ebe25a2a9
NaskoksaN <nbobchev@yahoo.com>
2025-01-26 09:06:56 +0200

    Set filter in GetAllAync optional.

--- Product.DataAccess/Repository/Interface/ISqlRepository.cs
+++ Product.DataAccess/Repository/Interface/ISqlRepository.cs
@@ -4,7 +4,7 @@ namespace ProductHub.DataAccess.Repository.Interface
 {
     public interface ISqlRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter, 
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter, 
                         string? includeProperties = null);
         Task<T> GetAsync(Expression<Func<T, bool>> filter, 
                         string? includeProperties = null, 
