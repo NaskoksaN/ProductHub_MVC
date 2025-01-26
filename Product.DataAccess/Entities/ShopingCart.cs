6451a4cbcbf7c83e817f33cf89dbad972d1502e2
NaskoksaN <nbobchev@yahoo.com>
2025-01-26 08:59:27 +0200

    Add entiteis model

---
+++ Product.DataAccess/Entities/ShopingCart.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductHub.DataAccess.Entities
+{
+    public class ShopingCart
+    {
+        [Key]
+        [Comment("Order Id")]
+        public int Id { get; set; }
+       
+        [Comment("Product id")]
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+        [Required]
+        [Comment("Count of product")]
+        public int Count { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; } = string.Empty;
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+    }
+}
