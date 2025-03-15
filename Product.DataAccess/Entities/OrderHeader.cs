65ce08f83d1ebe5f2be724ca833964edeb47bfcb
NaskoksaN <nbobchev@yahoo.com>
2025-03-15 14:24:46 +0200

    Add entity class OrderHeader

---
+++ Product.DataAccess/Entities/OrderHeader.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using static ProductHub.Models.Constants.DataConstants;
+
+namespace ProductHub.DataAccess.Entities
+{
+    public class OrderHeader
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+
+        [Comment("Order Date")]
+        public DateTime OrderDate { get; set; }
+
+        [Comment("Shipping Date")]
+        public DateTime ShippingDate { get; set; }
+
+        [Comment("Order Total")]
+        public double OrderTotal { get; set; }
+
+        [Comment("Order Status")]
+        public string? OrderStatus { get; set; }
+
+
+        [Comment("PaymentStatus")]
+        public string? PaymentStatus { get; set; }
+
+        [Comment("Tracking Number ")]
+        public string? TrackingNumber { get; set; }
+
+        [Comment("Carrier")]
+        public string? Carrier { get; set; }
+
+        [Comment("Payment Date")]
+        public DateTime PaymentDate { get; set; }
+
+        [Comment("Payment DueDate")]
+        public DateOnly PaymentDueDate { get; set; }
+
+        [Comment("Payment IntentId")]
+        public string? PaymentIntentId { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("User full name")]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(StreetMaxLength)]
+        [Comment("User street address")]
+        public string? StreetAddress { get; set; }
+
+        [MaxLength(CityMaxLength)]
+        [Comment("User city")]
+        public string? City { get; set; }
+
+        [MaxLength(PostCodeMaxLength)]
+        [Comment("User postal code")]
+        public string? PostalCode { get; set; }
+    }
+}
