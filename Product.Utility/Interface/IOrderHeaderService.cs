3f9ead877a7e61b6596fc34e935ee5e225737375
NaskoksaN <nbobchev@yahoo.com>
2025-03-15 14:41:18 +0200

    add Interface and Service for update OrderHeader

---
+++ Product.Utility/Interface/IOrderHeaderService.cs
@@ -0,0 +1,10 @@
+﻿using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository.Interface;
+
+namespace ProductHub.Utility.Interface
+{
+    public interface IOrderHeaderService : ISqlRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+    }
+}
---
+++ Product.Utility/Service/OrderHeaderService.cs
@@ -0,0 +1,21 @@
+﻿using ProductHub.DataAccess.Data;
+using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository;
+using ProductHub.Utility.Interface;
+
+namespace ProductHub.Utility.Service
+{
+    public class OrderHeaderService : SqlRepository<OrderHeader>, IOrderHeaderService
+    {
+        private readonly ApplicationDbContext db;
+        public OrderHeaderService(ApplicationDbContext _db) : base(_db)
+        {
+            db = _db;
+        }
+       
+        public void Update(OrderHeader obj)
+        {
+            db.OrderHeaders.Update(obj);
+        }
+    }
+}
