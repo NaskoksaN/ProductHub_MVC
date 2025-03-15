7a7647175c5405a3b630c01191c69a46d8d980d6
NaskoksaN <nbobchev@yahoo.com>
2025-03-15 14:24:33 +0200

    Add entity class OrderDetails

---
+++ Product.DataAccess/Entities/OrderDetail.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace ProductHub.DataAccess.Entities
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required]
+        public int OrderHeaderId {  get; set; }
+        [ForeignKey(nameof(OrderHeaderId))]
+        [Required]
+        public OrderHeader OrderHeader { get; set; } = null!;
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        [Required]
+        public Product Product { get; set; } = null!;
+
+        [Comment("Count")]
+        public int Count {  get; set; }
+
+        [Comment("Price")]
+        public double Price {  get; set; }
+
+    }
+}
