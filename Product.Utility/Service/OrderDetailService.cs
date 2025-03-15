48c4cbdf81cf501be0740ac397f7d5661cc0bece
NaskoksaN <nbobchev@yahoo.com>
2025-03-15 14:41:01 +0200

    Add Interface and Service for Update

---
+++ Product.Utility/Service/OrderDetailService.cs
@@ -0,0 +1,21 @@
+﻿using ProductHub.DataAccess.Data;
+using ProductHub.DataAccess.Entities;
+using ProductHub.DataAccess.Repository;
+using ProductHub.Utility.Interface;
+
+namespace ProductHub.Utility.Service
+{
+    public class OrderDetailService : SqlRepository<OrderDetail>, IOrderDetailService
+    {
+        private readonly ApplicationDbContext db;
+        public OrderDetailService(ApplicationDbContext _db) : base(_db)
+        {
+            db = _db;
+        }
+       
+        public void Update(OrderDetail obj)
+        {
+            db.OrderDetails.Update(obj);
+        }
+    }
+}
