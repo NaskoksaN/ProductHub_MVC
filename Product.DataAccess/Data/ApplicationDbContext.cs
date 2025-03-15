a48216e80a6c75a504468195848fb01817d6e677
NaskoksaN <nbobchev@yahoo.com>
2025-03-15 14:26:03 +0200

    add OrderHeader and OrderDetails tabels to Db

--- Product.DataAccess/Data/ApplicationDbContext.cs
+++ Product.DataAccess/Data/ApplicationDbContext.cs
@@ -19,8 +19,9 @@ namespace ProductHub.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
-
         public DbSet<ShopingCart> ShopingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
