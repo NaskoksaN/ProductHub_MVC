133c9db80fbdf16ab2a1449f871525a2ae21f7ff
NaskoksaN <nbobchev@yahoo.com>
2025-03-15 14:40:28 +0200

    add migration for OrderHeader and Details

---
+++ Product.DataAccess/Migrations/20250315122740_addOrderHeaderAndDetailsToDb.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductHub.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOrderHeaderAndDetailsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "Img Url",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "OrderHeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Order Date"),
+                    ShippingDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Shipping Date"),
+                    OrderTotal = table.Column<double>(type: "float", nullable: false, comment: "Order Total"),
+                    OrderStatus = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Order Status"),
+                    PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "PaymentStatus"),
+                    TrackingNumber = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Tracking Number "),
+                    Carrier = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Carrier"),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Payment Date"),
+                    PaymentDueDate = table.Column<DateOnly>(type: "date", nullable: false, comment: "Payment DueDate"),
+                    PaymentIntentId = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Payment IntentId"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "User full name"),
+                    StreetAddress = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true, comment: "User street address"),
+                    City = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: true, comment: "User city"),
+                    PostalCode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true, comment: "User postal code")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderHeaders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderHeaderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false, comment: "Count"),
+                    Price = table.Column<double>(type: "float", nullable: false, comment: "Price")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_OrderHeaders_OrderHeaderId",
+                        column: x => x.OrderHeaderId,
+                        principalTable: "OrderHeaders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderHeaderId",
+                table: "OrderDetails",
+                column: "OrderHeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHeaders_ApplicationUserId",
+                table: "OrderHeaders",
+                column: "ApplicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "OrderHeaders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "Img Url");
+        }
+    }
+}
