9292ac98e130813e386fe33026e1b170d2031106
NaskoksaN <nbobchev@yahoo.com>
2025-03-16 08:56:29 +0200

    add UpdateStatus and updateStatusPaymentId in OrderHeaderService

--- Product.Utility/Interface/IOrderHeaderService.cs
+++ Product.Utility/Interface/IOrderHeaderService.cs
@@ -6,5 +6,7 @@ namespace ProductHub.Utility.Interface
     public interface IOrderHeaderService : ISqlRepository<OrderHeader>
     {
         void Update(OrderHeader obj);
+        Task UpdateStatus(int id, string OrderStatus, string? paymentStatus=null);
+        Task UpdateStripePaymentId(int id, string sessionId, string paymentIntentId);
     }
 }
--- Product.Utility/Service/OrderHeaderService.cs
+++ Product.Utility/Service/OrderHeaderService.cs
@@ -1,4 +1,5 @@
-﻿using ProductHub.DataAccess.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductHub.DataAccess.Data;
 using ProductHub.DataAccess.Entities;
 using ProductHub.DataAccess.Repository;
 using ProductHub.Utility.Interface;
@@ -12,10 +13,38 @@ namespace ProductHub.Utility.Service
         {
             db = _db;
         }
-       
+
         public void Update(OrderHeader obj)
         {
             db.OrderHeaders.Update(obj);
+        }
+
+        public async Task UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = await db.OrderHeaders.FirstOrDefaultAsync(x => x.Id == id);
+            if ((orderFromDb != null))
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public async Task UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDb = await db.OrderHeaders.FirstOrDefaultAsync(x => x.Id == id);
+            if ((!string.IsNullOrEmpty(sessionId)))
+            {
+                orderFromDb.SeesionId = sessionId;
+
+                if ((!string.IsNullOrEmpty(paymentIntentId)))
+                {
+                    orderFromDb.PaymentIntentId = paymentIntentId;
+                    orderFromDb.PaymentDate = DateTime.Now;
+                }
+            }
         }
     }
 }
