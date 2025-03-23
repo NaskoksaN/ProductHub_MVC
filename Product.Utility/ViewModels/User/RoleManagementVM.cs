cbc0dd6bf9b56e939f01b37597d73f8b952b7a97
NaskoksaN <nbobchev@yahoo.com>
2025-03-23 09:33:27 +0200

    create RoleManagementView Model

---
+++ Product.Utility/ViewModels/User/RoleManagementVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ProductHub.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductHub.Utility.ViewModels.User
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+        
+    }
+}
