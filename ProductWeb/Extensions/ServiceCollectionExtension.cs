bfa3c0a8cc116e3cb8eb070a52516ede61b47e38
NaskoksaN <nbobchev@yahoo.com>
2025-03-23 11:28:54 +0200

    add service

--- ProductWeb/Extensions/ServiceCollectionExtension.cs
+++ ProductWeb/Extensions/ServiceCollectionExtension.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.
                 AddIdentity<IdentityUser, IdentityRole>(options =>
                 {
-                    options.SignIn.RequireConfirmedAccount = true;
+                    options.SignIn.RequireConfirmedAccount = false;
 
                     //only fo development
                     options.Password.RequireNonAlphanumeric = false;
